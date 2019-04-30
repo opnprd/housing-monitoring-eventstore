3aa680ff7c57646727af8202dfccb321dbbc44d8
Giles Dring <giles.dring@odileeds.org>
2019-04-30 18:52:46 +0100

    Add CORS middleware

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const restify = require('restify');
+const corsMiddleware = require('restify-cors-middleware');
+
 const logger = require('./services/logger');
 
 const server = restify.createServer({
@@ -6,13 +8,19 @@ const server = restify.createServer({
   log: logger,
 });
 
+const cors = corsMiddleware({
+  origins: ['http://localhost:*'],
+});
+
 server.pre(restify.plugins.pre.context());
+server.pre(cors.preflight);
 
 server.use(restify.plugins.requestLogger());
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.gzipResponse());
+server.use(cors.actual);
 
 server.on('after', restify.plugins.auditLogger({event: 'after', log: logger}));
 
