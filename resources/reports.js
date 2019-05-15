05ae73f1f5eca26743085a29b6dcc3e00eaff6c4
Giles Dring <giles.dring@odileeds.org>
2019-05-15 08:16:18 +0100

    Cache the report data

--- resources/reports.js
+++ resources/reports.js
@@ -1,5 +1,7 @@
 const mongodb = require('../services/mongo');
 
+let reportData = null;
+
 const aggregationPipeline = [
     {
       '$lookup': { from: 'events', localField: 'schemeId', foreignField: 'scheme', as: 'events' }
@@ -42,12 +44,24 @@ const aggregationPipeline = [
 const aggregationOptions = {};
 
 async function aggregateSchemes() {
+  // Terminate early if already populated
+  if (reportData) return reportData;
   const mongoClient = await mongodb.connect();
   const schemes = mongoClient.db().collection('schemes');
   const results = await schemes.aggregate(aggregationPipeline, aggregationOptions).toArray();
+  reportData = results;
   return results;
 }
 
+async function refreshCache() {
+  reportData = null;
+  await aggregateSchemes();
+}
+
+// Populate the data
+refreshCache();
+
 module.exports = {
   aggregateSchemes,
+  refreshCache,
 };
